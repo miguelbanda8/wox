1d1def16372d19708c6d1f2afc5f6d4c5b224e7f
Miguel Banda <banda.miguel@gmail.com>
2026-04-30 23:54:48 -0500

    Switch to charts_flutter_fork

--- lib/screens/DeviceInfo.dart
+++ lib/screens/DeviceInfo.dart
@@ -14,7 +14,7 @@ import 'package:gpspro/services/APIService.dart';
 import 'package:gpspro/theme/CustomColor.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:charts_flutter_fork/flutter.dart' as charts;
 import 'package:flutter/material.dart' as m;
 
 import '../store/device.dart';
