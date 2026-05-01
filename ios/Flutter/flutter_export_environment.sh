72c1350972b2e7507de948b0445bae5d8470365f
Miguel Banda <banda.miguel@gmail.com>
2026-05-01 00:24:11 -0500

    fix charts import{

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter_windows_3.24.1-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\TrackMi"
+export "FLUTTER_APPLICATION_PATH=C:\Users\mband\Desktop\TrackMiIOS"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
